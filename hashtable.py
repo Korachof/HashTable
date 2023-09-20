8eb5cf9b5e0a913c386adc2439dd3520c0a731c4
Christopher Partin <christopherpartin11@gmail.com>
2023-09-20 15:58:26 -0700

    Added docstring to __hash method

--- hashtable.py
+++ hashtable.py
@@ -3,9 +3,17 @@ class Hash:
         self.hash_map = [None] * size
 
     def __hash(self, key):
+        """
+        Runs a hash that will find a place in memory for the given key
+        :param key: The given key to be hashed
+        :return: the hash result
+        """
         hash_time = 0
         for letter in key:
             hash_time = (hash_time + ord(letter) * 31) % len(self.hash_map)
 
         return hash_time
+
+
+
 
